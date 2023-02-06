ad14b79a47d21c8a98d07b9e0679357b217a34d6
Ximenitap <123569520+Ximenitap@users.noreply.github.com>
2023-02-06 12:03:27 -0500

    gttgstg febrero 6

---
+++ app.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded",() => {
+const cardsAdj = [
+{
+  name: 'Vaquitas'
+  img: 'imagenes/vaca1.jpg'
+}
+{
+  name: 'Vaquitas'
+  img: 'imagenes/vaca2.jpg'
+}
+{
+  name: 'Vaquitas'
+  img: 'imagenes/simon.jpg'
+}
+{
+  name: 'Vaquitas'
+  img: 'imagenes/rossy.jpg'
+}
+{
+  name: 'Vaquitas'
+  img: 'imagenes/arroz y juli.jpg'
+}
+{
+  name: 'Vaquitas'
+  img: 'imagenes/vaca6.jpg'
+}
+
+]
+});
+
+
+
